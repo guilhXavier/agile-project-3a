6bca06a37778d1f13525205da59b80414e813aad
Eduardo Rigon <edukatiau@gmail.com>
2024-05-02 21:34:35 -0300

    add: adding Racha Repository

---
+++ src/main/java/ifsul/agileproject/rachadinha/repository/RachaRepository.java
@@ -0,0 +1,7 @@
+package ifsul.agileproject.rachadinha.repository;
+
+import ifsul.agileproject.rachadinha.domain.entity.Racha;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RachaRepository extends JpaRepository<Racha, Long> {
+}
