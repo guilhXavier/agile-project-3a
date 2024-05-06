c1251c12eadaa90e1460b7ac79fdcd07b598b714
Diego Dex <diegodexw@gmail.com>
2024-05-06 20:03:07 -0300

    chore: removed unused import

--- src/main/java/ifsul/agileproject/rachadinha/repository/UserRepository.java
+++ src/main/java/ifsul/agileproject/rachadinha/repository/UserRepository.java
@@ -2,9 +2,11 @@ package ifsul.agileproject.rachadinha.repository;
 
 import ifsul.agileproject.rachadinha.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
   Optional<User> findByEmail(String email);
