de1adaddfbe53ad76bed006eadaf8397cbcd9c7b
Diego Dex <diegodexw@gmail.com>
2024-05-06 20:01:40 -0300

    feat: add  findByStatus function on RachaRepository.java

--- src/main/java/ifsul/agileproject/rachadinha/repository/RachaRepository.java
+++ src/main/java/ifsul/agileproject/rachadinha/repository/RachaRepository.java
@@ -1,7 +1,13 @@
 package ifsul.agileproject.rachadinha.repository;
 
 import ifsul.agileproject.rachadinha.domain.entity.Racha;
-import org.springframework.data.jpa.repository.JpaRepository;
+import ifsul.agileproject.rachadinha.domain.entity.Status;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface RachaRepository extends JpaRepository<Racha, Long> {
+
+    Racha findByStatus(Status status);
 }
