b668457673aab9588429230f97d68f21ff2ba034
Diego Dex <diegodexw@gmail.com>
2024-05-06 19:52:58 -0300

    feat: add more fields to RachaDTO.java

--- src/main/java/ifsul/agileproject/rachadinha/domain/dto/RachaDTO.java
+++ src/main/java/ifsul/agileproject/rachadinha/domain/dto/RachaDTO.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import ifsul.agileproject.rachadinha.domain.entity.Status;
 import ifsul.agileproject.rachadinha.domain.entity.User;
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
 public class RachaDTO {
@@ -12,9 +13,10 @@ public class RachaDTO {
 	private Long id;
 	private String name;
 	private String description;
+	private Double goal;
 	private User owner;
 	private Status status;
 	private Date created_at;
 	private String inviteLink;
-
+	private String password;
 }
