121d21f983f833de5c2e23e523a3d1586efd1939
Diego Dex <diegodexw@gmail.com>
2024-05-06 20:06:10 -0300

    feat: add RachaService.java

---
+++ src/main/java/ifsul/agileproject/rachadinha/service/RachaService.java
@@ -0,0 +1,21 @@
+package ifsul.agileproject.rachadinha.service;
+
+import java.util.Optional;
+
+import ifsul.agileproject.rachadinha.domain.dto.RachaDTO;
+import ifsul.agileproject.rachadinha.domain.entity.Racha;
+import ifsul.agileproject.rachadinha.domain.entity.Status;
+
+public interface RachaService {
+    
+    Racha saveRacha(RachaDTO rachaDTO);
+
+    Optional<Racha> findRachaById(Long id);
+
+    void deleteRachaById(Long id);
+
+    Racha updateRacha(RachaDTO rachaDTO);
+
+    Racha findRachaByStatus(Status status);
+    
+}
