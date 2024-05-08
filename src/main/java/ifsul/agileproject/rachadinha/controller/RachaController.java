030de9b5fc9328450a103efbb6bbf36120b04649
Eduardo Rigon <70084379+edukatiau@users.noreply.github.com>
2024-05-08 15:57:02 +0000

    feat: create RachaController and 'create' function

---
+++ src/main/java/ifsul/agileproject/rachadinha/controller/RachaController.java
@@ -0,0 +1,29 @@
+package ifsul.agileproject.rachadinha.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ifsul.agileproject.rachadinha.domain.dto.RachaRegisterDTO;
+import ifsul.agileproject.rachadinha.domain.entity.Racha;
+import ifsul.agileproject.rachadinha.service.impl.RachaServiceImpl;
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/racha")
+@AllArgsConstructor
+public class RachaController {
+    
+    private final RachaServiceImpl rachaService;
+
+    //Criar racha
+    @PostMapping("/criar")
+    public ResponseEntity<Racha> saveUser(@RequestBody RachaRegisterDTO rachaDTO) {
+        Racha racha = rachaService.saveRacha(rachaDTO);
+        return new ResponseEntity<>(racha, HttpStatus.CREATED);
+    }
+
+}
