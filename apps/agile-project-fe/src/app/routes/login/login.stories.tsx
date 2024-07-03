ead576c1cf4f70629143faeb48976f55e959ae53
Matheus Moraes <116031835+m-matheus@users.noreply.github.com>
2024-07-02 21:20:38 -0300

    feat: create login page

--- apps/agile-project-fe/src/app/routes/login/login.stories.tsx
+++ apps/agile-project-fe/src/app/routes/login/login.stories.tsx
@@ -1,5 +1,18 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import { Login } from './login';
+import { queryClient } from '../../api';
 
-export default { component: Login, title: 'Login Page', tags: ['autodocs'] };
+export default {
+  component: Login,
+  title: 'Login Page',
+  tags: ['autodocs'],
+  decorators: [
+    (Story: React.FC): React.ReactElement => (
+      <QueryClientProvider client={queryClient}>
+        <Story />
+      </QueryClientProvider>
+    ),
+  ],
+};
 
 export const Primary = {};
