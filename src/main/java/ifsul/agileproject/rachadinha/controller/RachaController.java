167a1d55b30c60f562e870a6f9c7e8a85e92737f
brufreitasmo <brunafreitasmotta@gmail.com>
2024-07-05 18:41:48 -0300

    feat: added delete racha

--- src/main/java/ifsul/agileproject/rachadinha/controller/RachaController.java
+++ src/main/java/ifsul/agileproject/rachadinha/controller/RachaController.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ifsul.agileproject.rachadinha.domain.dto.RachaDTO;
 import ifsul.agileproject.rachadinha.domain.dto.RachaRegisterDTO;
 import ifsul.agileproject.rachadinha.domain.entity.Racha;
 import ifsul.agileproject.rachadinha.domain.entity.User;
@@ -50,4 +52,21 @@ public class RachaController {
 
 		return new ResponseEntity<>(rachasResponseDto, HttpStatus.OK);
 	}
+
+	
+	@DeleteMapping("/{idRacha}/owner/{idOwner}")
+
+	public ResponseEntity deleteRachaByID(@PathVariable Long idRacha, @PathVariable Long idOwner) {
+	
+		Racha racha = rachaService.findRachaById(idRacha).get();
+
+	if (racha.getOwner().getId() != idOwner){
+		return new ResponseEntity("Usuário não autorizado", HttpStatus.FORBIDDEN);
+	}
+
+     rachaService.deleteRachaById(racha.getId());
+        return new ResponseEntity("Racha deletado", HttpStatus.OK);
+	
+    }
 }
+
