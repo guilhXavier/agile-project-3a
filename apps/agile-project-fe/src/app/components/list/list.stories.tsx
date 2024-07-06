b7585231452fa05b9f99e4eda12d6015fb10b282
Matheus Moraes <116031835+m-matheus@users.noreply.github.com>
2024-07-05 22:35:55 -0300

    feat: list story (not done yet)

--- apps/agile-project-fe/src/app/components/list/list.stories.tsx
+++ apps/agile-project-fe/src/app/components/list/list.stories.tsx
@@ -1,12 +1,21 @@
 /* eslint-disable */
-import List from './list';
+import List, { ListProps } from './list';
 
 export default {
+  component: List,
   title: 'List',
+  tags: ['autodocs'],
+  argTypes: {
+    items: {
+      control: 'object',
+      type: { name: 'Array<ChipIn>', required: true },
+      description: 'List of items',
+      defaultValue: [
+        { name: 'ChipIn 1', amount: 100, user: { name: 'User 1' } },
+        { name: 'ChipIn 2', amount: 200, user: { name: 'User 2' } },
+      ],
+    },
+  },
 };
 
-export const Default = () => <List />;
-
-Default.story = {
-  name: 'default',
-};
+export const Primary = ({ items = [] }: ListProps) => <List items={items} />;
