20f636665b84765a8c1f90a1b0874f882d0742d1
Matheus Moraes <116031835+m-matheus@users.noreply.github.com>
2024-07-09 13:11:18 -0300

    feat: styled list

--- apps/agile-project-fe/src/app/components/list/list.tsx
+++ apps/agile-project-fe/src/app/components/list/list.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import { ChipIn } from '../../types';
+import { StyledList } from './list.styled';
 
 export interface ListProps {
   items: Array<ChipIn>;
 }
 
 export const List: React.FC<ListProps> = ({ items }) => (
-  <ul>
-    {Array.isArray(items) ? (
-      items.map((item, index) => (
-        <li key={index}>
-          <p>Id: {item.id}</p>
-          <p>User: {item.user.name}</p>
-          <p>Amount: {item.amount}</p>
-        </li>
-      ))
-    ) : (
-      <li>No items to display</li>
-    )}
-  </ul>
+  <StyledList>
+    <ul>
+      {Array.isArray(items) ? (
+        items.map((item, index) => (
+          <li key={index}>
+            <p>Id: {item.id}</p>
+            <p>User: {item.user.name}</p>
+            <p>Amount: {item.amount}</p>
+          </li>
+        ))
+      ) : (
+        <li>No items to display</li>
+      )}
+    </ul>
+  </StyledList>
 );
 
 export default List;
