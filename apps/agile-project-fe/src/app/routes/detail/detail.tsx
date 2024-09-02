ba4199e4c9e46deb20ed7cc2f64d5c45b31b342f
guilherme.xavier <guilhermetamara5@icloud.com>
2024-09-02 20:35:10 -0300

    feat: added detail page skeleton

--- apps/agile-project-fe/src/app/routes/detail/detail.tsx
+++ apps/agile-project-fe/src/app/routes/detail/detail.tsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useGetChipInById } from '../../api/useGetChipInById/useGetChipInById';
+import styled from 'styled-components';
+import { SideMenu } from '../../components/side-menu/side-menu';
+import { Card } from '../../components/card/card';
+
+const StyledDetail = styled.div``;
 
 export const Detail: React.FC = () => {
-  return <div>Detail goes here</div>;
+  const { chipInId } = useParams();
+
+  const { data, isLoading, isSuccess } = useGetChipInById(chipInId || '');
+
+  return (
+    <StyledDetail>
+      <SideMenu />
+      {isLoading && <p>Loading...</p>}
+      {isSuccess && (
+        <Card key={data?.id || ''}>
+          <h2>{data?.name}</h2>
+        </Card>
+      )}
+    </StyledDetail>
+  );
 };
